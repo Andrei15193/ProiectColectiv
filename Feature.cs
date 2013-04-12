dd9b26e4eeda23221b90b27f6ffd9d4d92edb8d9
Mihaltan Alexandra <alexa_mihaltan@yahoo.com>
2013-04-12 22:59:18 +0300

    UsersEntities

---
+++ Feature.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    class Feature
+    {
+        private string name;
+        private string description;
+        public Feature(string name,string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+        public Feature()
+        {
+            this.name="";
+            this.description="";
+        }
+
+        public string getName()
+        {
+        return name;
+        }
+        public string getDescription()
+        {
+            return description;
+        }
+       public override bool Equals(Object obj)
+   {
+      Feature featureObj = obj as Feature; 
+      if (featureObj == null)
+         return false;
+      else 
+         return name.Equals(featureObj.getName());
+   }
+        public override string ToString()
+   {
+      return this.name;
+   }
+
+
+        }
+    
+

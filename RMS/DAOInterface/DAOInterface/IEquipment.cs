b0f7d7d18bd4cfa4bfebac3d16c9735ee1624653
eduardb <edy@fellan-soft.com>
2013-05-16 23:36:56 +0300

    Added LogisticalResourceViewModel and afferent DA interfaces

---
+++ RMS/DAOInterface/DAOInterface/IEquipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAOInterface.DAOInterface
+{
+    public interface IEquipment
+    {
+        void Add(ResourceManagementSystem.BusinessLogic.Entities.Equipment equipment);
+        void Update(string serialNumber, ResourceManagementSystem.BusinessLogic.Entities.Equipment newEquipment);
+        void Delete(string serialNumber);
+    }
+}

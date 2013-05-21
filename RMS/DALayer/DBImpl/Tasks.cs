7477a61a6fd928072eb73af0a9b835f7296e5aad
unknown <alexandruio@AlexandruIo-lap.yonder.local>
2013-05-21 17:06:36 +0300

    Added GetIdTaskByTask(the task needs to have a valid location).

---
+++ RMS/DALayer/DBImpl/Tasks.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL = ResourceManagementSystem.BusinessLogic.Entities;
+
+namespace DALayer.DBImpl
+{
+    public class Tasks
+    {
+        public int? GetTaksIdByTask(BL.ITask task)
+        {
+            if (task == null || task.Location == null || task.StartDate != null || task.Location.Building == null)
+            {
+                return null;
+            }
+
+            int? taskId = null;
+
+            DBConnection dbConnection = new DBConnection();
+
+            SqlCommand cmd = new SqlCommand(@"select t.id from (select * from ClassRooms as cr where cr.buildingName = @building and classFloor = @floor and number = @number) as c 
+                                                inner join TasksClassRooms as tcr on c.id = tcr.classRoomId 
+                                                inner join Tasks as t on t.id = tcr.taskId where t.startDate = @startdate", dbConnection.Connection);
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@building",
+                Value = task.Location.Building
+            });
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@number",
+                Value = Convert.ToInt32(task.Location.Number)
+            });
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@floor",
+                Value = task.Location.Floor
+            });
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                ParameterName = "@startdate",
+                Value = task.StartDate
+            });
+
+            dbConnection.Connection.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                if (!dr.IsDBNull(0))
+                {
+                    taskId = dr.GetInt32(0);
+                }
+            }
+
+            dr.Close();
+            dbConnection.Connection.Close();
+
+            return taskId;
+        }
+    }
+}
---
+++ RMS/DATest/DATests/TasksTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALayer.DBImpl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BL = ResourceManagementSystem.BusinessLogic.Entities;
+
+namespace DATest.DATests
+{
+    [TestClass]
+    public class TasksTest
+    {
+        [TestMethod]
+        public void GetTaskIdByTaskTest()
+        {
+            BL.Role role = new BL.Role("Admin", new BL.Feature[1] { new BL.Feature("useless feature")});
+            BL.Member member = new BL.Member(role, "George", "Popa", "asdas@abc.com", "sadasda");
+            BL.Course course = new BL.Course("Informatica", 6, BL.Language.Romanian, BL.ApplicationDomain.ComputerScience);
+            BL.ClassRoom classRoom = new BL.ClassRoom("Sediul central", 1, 2);
+            BL.FinancialResource financialResource = new BL.FinancialResource(23, BL.Currency.RON);
+            BL.DidacticTask didacticTask = new BL.DidacticTask(course, "descr", new DateTime(2013, 05, 21), 2, BL.ClassType.Course, 
+                                                               classRoom, financialResource, member, 
+                                                               new BL.GroupFormation(BL.Specialization.ComputerScience, 2, 221));
+
+            //This is not good. Doing a declaration like that just to test a minor thing.
+
+            Tasks tasks = new Tasks();
+            int? id = tasks.GetTaksIdByTask(didacticTask);
+            Assert.IsTrue(id.HasValue);
+            Assert.AreEqual(id.Value, 1);
+        }
+    }
+}

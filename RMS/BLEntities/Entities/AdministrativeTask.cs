5cc144aba7a31091bb806e8ab63812285e89cef6
eduardb <edy@fellan-soft.com>
2013-05-25 19:06:18 +0300

    Added administrative task and view model

---
+++ RMS/BLEntities/Entities/AdministrativeTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceManagementSystem.BusinessLogic.Entities
+{
+    public class AdministrativeTask : AbstractTask
+    {
+        public AdministrativeTask(string title, string description, DateTime startDate, DateTime endDate, string typeName, ClassRoom location, FinancialResource estimatedBudget, IEnumerable<Member> assignees) :
+            base(title, description, startDate, endDate, "administrative", location, estimatedBudget, assignees)
+        { }
+    }
+}

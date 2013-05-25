07cb3cba3575c3c62393fbae0595a153a2139304
eduardb <edy@fellan-soft.com>
2013-05-25 19:33:36 +0300

    Added meeting task

--- RMS/BLEntities/Entities/AdministrativeTask.cs
+++ RMS/BLEntities/Entities/AdministrativeTask.cs
@@ -8,7 +8,7 @@ namespace ResourceManagementSystem.BusinessLogic.Entities
 {
     public class AdministrativeTask : AbstractTask
     {
-        public AdministrativeTask(string title, string description, DateTime startDate, DateTime endDate, string typeName, ClassRoom location, FinancialResource estimatedBudget, IEnumerable<Member> assignees) :
+        public AdministrativeTask(string title, string description, DateTime startDate, DateTime endDate, ClassRoom location, FinancialResource estimatedBudget, IEnumerable<Member> assignees) :
             base(title, description, startDate, endDate, "administrative", location, estimatedBudget, assignees)
         { }
     }
---
+++ RMS/BLEntities/Entities/MeetingTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceManagementSystem.BusinessLogic.Entities
+{
+    class MeetingTask : AbstractTask
+    {
+        public MeetingTask(string title, string description, DateTime startDate, DateTime endDate, ClassRoom location, FinancialResource estimatedBudget, IEnumerable<Member> assignees)
+            : base(title, description, startDate, endDate, "meeting", location, estimatedBudget, assignees)
+        {
+        }
+    }
+}

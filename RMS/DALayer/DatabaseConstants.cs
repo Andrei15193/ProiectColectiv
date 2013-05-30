5837ee2c9b82419c0f38ec037683e8788957958c
Andrei Fangli <andrei_fangli@hotmail.com>
2013-05-30 10:25:10 +0300

    Changes to DB Constants

--- RMS/DALayer/DatabaseConstants.cs
+++ RMS/DALayer/DatabaseConstants.cs
@@ -12,7 +12,7 @@ namespace ResourceManagementSystem.DataAccess
             }
         }
 
-        private static readonly SqlConnection connection = new SqlConnection("Server=ANDREI-DESKTOP; Database=AndreiLocal; Trusted_Connection=True;");
+        private static readonly SqlConnection connection = new SqlConnection("Server=ANDREI-NETBOOK; Database=AndreiLocal; Trusted_Connection=True;");
         //private static readonly SqlConnection connection = new SqlConnection("Initial Catalog=Andrei15193_sqldb; Data Source=andrei15193.tk,779; User=Andrei15193_RMSuser; Password=d3m3nt14l123");
     }
 }

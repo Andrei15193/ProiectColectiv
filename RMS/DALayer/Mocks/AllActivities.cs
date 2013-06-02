cc9c09a4a0935730c52efed531f1530f117ca541
Mihaltan Alexandra <alexa_mihaltan@yahoo.com>
2013-06-02 12:09:21 +0300

    by works, i meant doesn't work. works now :)

--- RMS/DALayer/Mocks/AllActivities.cs
+++ RMS/DALayer/Mocks/AllActivities.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ResourceManagementSystem.BusinessLogic.Entities;
+using ResourceManagementSystem.BusinessLogic.Entities.Collections;
 using ResourceManagementSystem.DAOInterface;
 
 namespace ResourceManagementSystem.DataAccess.Mocks
@@ -14,6 +16,11 @@ namespace ResourceManagementSystem.DataAccess.Mocks
         {
             activities = new HashSet<AbstractActivity>();
             activities.Add(new DidacticActivity(CourseType.Course, "Algoritmica grafelor", "Ceva linii si cercuri", "I2", DateTime.Now.AddDays(-10), DateTime.Now, new Teacher(TeachingPosition.Professor, "Andrei", "fair1064@scs.ubbcluj.ro", "123456", "Stuff")));
+           
+           
+
+            activities.Add( new AdministrativeActivity("titlue","desc",DateTime.Now.AddDays(-10), DateTime.Now));
+            activities.Add(new ResearchProject("research project title", "mega descrire", DateTime.Now.AddDays(-10), DateTime.Now,null));
         }
 
         public IEnumerable<AbstractActivity> AsEnumerable
@@ -25,5 +32,16 @@ namespace ResourceManagementSystem.DataAccess.Mocks
         }
 
         private ICollection<AbstractActivity> activities;
+
+
+        public IEnumerable<AbstractActivity> getAllPendingActivities()
+        {
+            return activities;
+        }
+
+        public void aproveActivity(AbstractActivity activity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- RMS/DAOInterface/IAllActivities.cs
+++ RMS/DAOInterface/IAllActivities.cs
@@ -6,5 +6,7 @@ namespace ResourceManagementSystem.DAOInterface
     public interface IAllActivities
     {
         IEnumerable<AbstractActivity> AsEnumerable { get; }
+        IEnumerable<AbstractActivity> getAllPendingActivities();
+        void aproveActivity(AbstractActivity activity);
     }
 }
